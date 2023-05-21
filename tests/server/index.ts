8c05e536d7e0e1829a0b6a2b20388ceda75c9c52
asplunds <71442467+asplunds@users.noreply.github.com>
2023-05-21 14:46:39 +0200

    chore(tests): improve dev env

--- tests/server/index.ts
+++ tests/server/index.ts
@@ -101,7 +101,7 @@ const requestListener = (
 
 const server = http.createServer(requestListener);
 
-server.listen(8000, () => {
+server.listen(8001, () => {
   console.log("Node backend started");
 });
 
