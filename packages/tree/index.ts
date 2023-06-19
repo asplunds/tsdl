ec5494d9f61215fafd7eb5abfb1c705cd308a3f1
asplunds <71442467+asplunds@users.noreply.github.com>
2023-06-19 11:51:10 +0200

    chore(docs): document auto docs

--- packages/tree/index.ts
+++ packages/tree/index.ts
@@ -2,11 +2,17 @@ import { types } from "@tsdl/core";
 import chalk from "chalk";
 
 type Tree = {
+  /** the complete path to the leaf */
   path: string[];
+  /** child nodes, will be empty if leaf */
   nodes: Tree[];
+  /** if true, this is a query (leaf node) */
   leaf: boolean;
+  /** documentation of the input */
   inputDoc?: types.routing.CommonDoc;
+  /** documentation of the query */
   queryDoc?: types.routing.CommonDoc;
+  /** documentation of the middlewares in order */
   mwDoc: types.routing.CommonDoc[];
 };
 
