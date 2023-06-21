92ee8efca013489fc096f7b4027677174f81358b
asplunds <71442467+asplunds@users.noreply.github.com>
2023-06-21 10:32:11 +0200

    chore(repo): type checking

--- tests/createQuery.test.ts
+++ tests/createQuery.test.ts
@@ -2,16 +2,18 @@ import { expect, test } from "vitest";
 import createQuery from "../packages/server/lib/createQuery";
 import { types } from "../packages/core";
 
-const queries = [[{undefined}, [{}] as types.routing.MiddlewareCollection[]]] as const;
+const queries = [
+  [{ undefined }, [{}] as types.routing.MiddlewareCollection[]],
+] as const;
 
 test.each(queries)("create queries should be correct", (validator, mw) => {
-  const created = createQuery(validator, mw, {name: null, description: null});
+  const created = createQuery(validator, mw, { name: null, description: null });
   expect(created).toHaveProperty("query");
   expect(created).toBeDefined();
 });
 
 test.each(queries)("queries should be correct", (validator, mw) => {
-  const created = createQuery(validator, mw, {name: null, description: null});
+  const created = createQuery(validator, mw, { name: null, description: null });
 
   const arg = () => 123;
   const query = created.query(arg);
